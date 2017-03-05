906ef837434534209e21af5846cfdec5ab273fb9
JUAN-PC\JuanManuel <juan211189@gmail.com>
2017-03-05 21:41:20 +0100

    Revert "Update working project"
    This reverts commit f93f2d71f1ff4e1c97af9ce0b8904dbd5fccf882.

--- src/main/scala-2.10/SVM.scala
+++ src/main/scala-2.10/SVM.scala
@@ -1,8 +1,7 @@
 import org.apache.spark.SparkContext
-import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
 
 /**
   * Created by Juan on 12/11/2016.
@@ -19,23 +18,7 @@ object SVM {
 
     // Run training algorithm to build the model
     val numIterations = 100
-
-    val svmP = new SVMWithSGD()
-    svmP.optimizer
-      .setNumIterations(numIterations)
-      .setRegParam(0.1)
-      .setUpdater(new AdaGrad())
-      //.setUpdater(new ConstantRate)
-
-    // Generate initial weights
-    val values = Array.fill(54)(0.5)
-    val initialWeights: Vector = Vectors.dense(values)
-
-    // Generate SVM model and measure execution time
-    val _startTime: Long = System.currentTimeMillis
-    //val model = SVMWithSGD.train(training, numIterations)
-    val model = svmP.run(training)
-    val _endTime = System.currentTimeMillis
+    val model = SVMWithSGD.train(training, numIterations)
 
     // Clear the default threshold.
     model.clearThreshold()
@@ -46,60 +29,16 @@ object SVM {
       (score, point.label)
     }
 
-    // Instantiate metrics object
+    // Get evaluation metrics.
     val metrics = new BinaryClassificationMetrics(scoreAndLabels)
-
-    // AUROC
     val auROC = metrics.areaUnderROC()
+
     println("Area under ROC = " + auROC)
 
-    // Processing time
-    println("Processing time " + ((_endTime - _startTime) / 1000) + " seconds.")
-    println("Processing time " + ((_endTime - _startTime)) + " milliseconds.")
-
-    /*
-    // Precision by threshold
-    val precision = metrics.precisionByThreshold
-    precision.foreach { case (t, p) =>
-      println(s"Threshold: $t, Precision: $p")
-    }
-
-    // Recall by threshold
-    val recall = metrics.recallByThreshold
-    recall.foreach { case (t, r) =>
-      println(s"Threshold: $t, Recall: $r")
-    }
-
-    // F-measure
-    val f1Score = metrics.fMeasureByThreshold
-    f1Score.foreach { case (t, f) =>
-      println(s"Threshold: $t, F-score: $f, Beta = 1")
-    }
-
-    val beta = 0.5
-    val fScore = metrics.fMeasureByThreshold(beta)
-    f1Score.foreach { case (t, f) =>
-      println(s"Threshold: $t, F-score: $f, Beta = 0.5")
-    }
-
-    // Precision-Recall Curve
-    val PRC = metrics.pr
-    println("PRC: " + PRC)
-
-    // AUPRC
-    val auPRC = metrics.areaUnderPR
-    println("Area under precision-recall curve = " + auPRC)
-
-    // Compute thresholds used in ROC and PR curves
-    val thresholds = precision.map(_._1)
-
-    // ROC Curve
-    val roc = metrics.roc
-    */
-
     // Save and load model
-    //model.save(sc, "target/tmp/scalaSVMWithSGDModel")
-    //val sameModel = SVMModel.load(sc, "target/tmp/scalaSVMWithSGDModel")
+    model.save(sc, "target/tmp/scalaSVMWithSGDModel")
+    val sameModel = SVMModel.load(sc, "target/tmp/scalaSVMWithSGDModel")
+    // $example off$
 
     sc.stop()
   }
