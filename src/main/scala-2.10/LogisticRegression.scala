56b6632bd23516167dd8cdc23312158fde1046a4
JUAN-PC\JuanManuel <juan211189@gmail.com>
2017-03-05 21:11:29 +0100

    Update working project

--- src/main/scala-2.10/LogisticRegression.scala
+++ src/main/scala-2.10/LogisticRegression.scala
@@ -13,17 +13,23 @@ object LogisticRegression {
     // Load training data in LIBSVM format.
     val data = MLUtils.loadLibSVMFile(sc, filePath)
 
-    // Split data into training (60%) and test (40%).
-    val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+    // Split data into training (70%) and test (30%).
+    val splits = data.randomSplit(Array(0.7, 0.3), seed = 11L)
     val training = splits(0).cache()
     val test = splits(1)
 
-    // Run training algorithm to build the model
     val numIterations = 100
-    val model = LogisticRegressionWithSGD.train(training, numIterations)
-    /*val model = new LogisticRegressionWithLBFGS()
-      .setNumClasses(10)
-      .run(training)*/
+    val lRegP = new LogisticRegressionWithLBFGS()
+    lRegP.optimizer
+      .setNumIterations(numIterations)
+      .setRegParam(0.1)
+      //.setUpdater(new AdaGrad(0))
+
+    // // Generate Logistic Regression model and measure execution time
+    val _startTime: Long = System.currentTimeMillis
+    val model = lRegP.run(training)
+    val _endTime = System.currentTimeMillis
+
 
     // Compute raw scores on the test set.
     val predictionAndLabels = test.map { case LabeledPoint(label, features) =>
@@ -36,11 +42,16 @@ object LogisticRegression {
     val accuracy = metrics.accuracy
     println(s"Accuracy = $accuracy")
 
+    // Processing time
+    println("Processing time " + ((_endTime - _startTime) / 1000) + " seconds.")
+    println("Processing time " + ((_endTime - _startTime)) + " milliseconds.")
+
+    /*
     // Save and load model
     model.save(sc, "target/tmp/scalaLogisticRegressionWithLBFGSModel")
     val sameModel = LogisticRegressionModel.load(sc,
       "target/tmp/scalaLogisticRegressionWithLBFGSModel")
-
+    */
     sc.stop()
   }
 }
--- src/main/scala-2.10/Main.scala
+++ src/main/scala-2.10/Main.scala
@@ -3,21 +3,23 @@ import java.io.File
 import org.apache.commons.io.FileUtils
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.mllib.util.MLUtils
 
 /**
   * Created by Juan on 12/11/2016.
   */
 object Main {
   def main(args: Array[String]): Unit = {
-    FileUtils.cleanDirectory(new File("C:/Users/Juan/git/LearningRate/target/tmp/"))
+    FileUtils.cleanDirectory(new File("C:/Users/JuanManuel/git/LearningRate/target/tmp/"))
 
-    val conf = new SparkConf().setAppName("SVMWithSGDExample").setMaster("local")
+    val conf = new SparkConf().setAppName("LearningRate").setMaster("local")
     val sc = new SparkContext(conf)
 
-    val filePath = "inFiles/sample_libsvm_data.txt"
-    LogisticRegression.runLogisticRegression(sc, filePath)
-    //SVM.runSVM(sc, filePath);
+    //val filePath = "inFiles/kddb-raw-libsvm"
+    //val filePath = "inFiles/covtype.txt"
+    //val filePath = "inFiles/sample_libsvm_data.txt"
+    val filePath = "inFiles/a9a.txt"
+
+    //LogisticRegression.runLogisticRegression(sc, filePath)
+    SVM.runSVM(sc, filePath);
   }
 }
